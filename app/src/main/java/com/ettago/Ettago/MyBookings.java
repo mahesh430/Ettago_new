57aa58a76c3b88e7c488d464e42131abcd484e8f
srinivasbandaru <srinivaschakravarthybandaru@urbancompany.com>
2022-11-24 14:29:22 +0530

    new chnages

---
+++ app/src/main/java/com/ettago/Ettago/MyBookings.java
@@ -0,0 +1,14 @@
+package com.ettago.Ettago;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyBookings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_bookings);
+    }
+}
